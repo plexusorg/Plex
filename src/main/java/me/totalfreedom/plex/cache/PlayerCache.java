a2f7394c88f281292e3c59aeb70aaf1ac308b28b
spacerocket62 <spacerocket62@gmail.com>
2020-10-28 13:47:00 -0700

    a

--- src/main/java/me/totalfreedom/plex/cache/PlayerCache.java
+++ src/main/java/me/totalfreedom/plex/cache/PlayerCache.java
@@ -20,4 +20,5 @@ public class PlayerCache
     {
         return plexPlayerMap;
     }
+    
 }
--- src/main/java/me/totalfreedom/plex/util/PlexUtils.java
+++ src/main/java/me/totalfreedom/plex/util/PlexUtils.java
@@ -1,8 +1,16 @@
 package me.totalfreedom.plex.util;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.Lists;
 import me.totalfreedom.plex.Plex;
 import me.totalfreedom.plex.storage.StorageType;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.PluginCommandYamlParser;
+import org.bukkit.plugin.Plugin;
 
 public class PlexUtils
 {
@@ -30,6 +38,26 @@ public class PlexUtils
         {
             PlexLog.log("Successfully enabled MongoDB!");
         }
+    }
+
+    public static boolean isPluginCMD(String cmd, String pluginName)
+    {
+        Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin(pluginName);
+        if (plugin == null)
+        {
+            PlexLog.error(pluginName + " can not be found on the server! Make sure it is spelt correctly!");
+            return false;
+        }
+        List<Command> cmds = PluginCommandYamlParser.parse(plugin);
+        for (Command pluginCmd : cmds)
+        {
+            List<String> cmdAliases = pluginCmd.getAliases().size() > 0 ? pluginCmd.getAliases().stream().map(String::toLowerCase).collect(Collectors.toList()) : null;
+            if (pluginCmd.getName().equalsIgnoreCase(cmd) || (cmdAliases != null && cmdAliases.contains(cmd.toLowerCase())))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
