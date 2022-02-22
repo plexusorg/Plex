c64d04b9a37e934dfbe74a053e199efd8ab39bbd
Telesphoreo <me@telesphoreo.me>
2022-02-22 00:51:46 -0600

    no

--- src/main/java/dev/plex/rank/enums/Rank.java
+++ src/main/java/dev/plex/rank/enums/Rank.java
@@ -16,7 +16,7 @@ public enum Rank
 {
     IMPOSTOR(-1, ChatColor.AQUA + "an " + ChatColor.YELLOW + "Impostor", "Impostor", "&8[&eImp&8]"),
     NONOP(0, "a " + ChatColor.WHITE + "Non-Op", "Non-Op", ""),
-    OP(1, "an " + ChatColor.GREEN + "Operator", "Operator", "&8[&aOp&8]"),
+    OP(1, "an " + ChatColor.GREEN + "Operator", "Operator", "&8[&aOP&8]"),
     ADMIN(2, "an " + ChatColor.DARK_GREEN + "Admin", "Admin", "&8[&2Admin&8]"),
     SENIOR_ADMIN(3, "a " + ChatColor.GOLD + "Senior Admin", "Senior Admin", "&8[&6SrA&8]"),
     EXECUTIVE(4, "an " + ChatColor.RED + "Executive", "Executive", "&8[&cExec&8]");
