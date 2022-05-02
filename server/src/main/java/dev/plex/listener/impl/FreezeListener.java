e484130d84e3f07ab8b9de6040f0b667b991a126
Business Goose <arclicious@vivaldi.net>
2022-05-02 06:08:30 +0100

    Cancel PlayerTeleportEvent for frozen players (closes #31)

--- server/src/main/java/dev/plex/listener/impl/FreezeListener.java
+++ server/src/main/java/dev/plex/listener/impl/FreezeListener.java
@@ -5,11 +5,22 @@ import dev.plex.listener.PlexListener;
 import dev.plex.player.PlexPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class FreezeListener extends PlexListener
 {
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e)
+    {
+        PlexPlayer player = DataUtils.getPlayer(e.getPlayer().getUniqueId());
+        if (player.isFrozen())
+        {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerTeleport(PlayerTeleportEvent e)
     {
         PlexPlayer player = DataUtils.getPlayer(e.getPlayer().getUniqueId());
         if (player.isFrozen())
