c9d954d1c017e1ffb14d1af6bd9403fc19e248bd
Telesphoreo <me@telesphoreo.me>
2024-01-19 22:30:26 -0600

    reformat

--- server/src/main/java/dev/plex/command/impl/MobPurgeCMD.java
+++ server/src/main/java/dev/plex/command/impl/MobPurgeCMD.java
@@ -24,39 +24,49 @@ import java.util.Collections;
 import java.util.List;
 
 @CommandPermissions(permission = "plex.mobpurge", source = RequiredCommandSource.ANY)
-@CommandParameters(name = "mobpurge", description = "Purge all mobs.", usage = "/<command> <mob>", aliases = "mp")
-public class MobPurgeCMD extends PlexCommand {
-
-    public static final List<EntityType> MOB_TYPES = new ArrayList<>();
+@CommandParameters(name = "mobpurge", description = "Purge all mobs.", usage = "/<command> [mob]", aliases = "mp")
+public class MobPurgeCMD extends PlexCommand
+{
+    private final List<EntityType> MOB_TYPES = new ArrayList<>();
 
     @Override
-    protected Component execute(@NotNull CommandSender sender, @Nullable Player playerSender, @NotNull String[] args) {
+    protected Component execute(@NotNull CommandSender sender, @Nullable Player playerSender, @NotNull String[] args)
+    {
         EntityType type = null;
         String mobName = null;
-        if (args.length > 0) {
-            try {
+        if (args.length > 0)
+        {
+            try
+            {
                 type = EntityType.valueOf(args[0].toUpperCase());
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 PlexLog.debug("A genius tried and failed removing the following invalid mob: " + args[0].toUpperCase());
                 send(sender, messageComponent("notAValidMob"));
                 return null;
             }
-            if (!MOB_TYPES.contains(type)) {
+            if (!MOB_TYPES.contains(type))
+            {
                 PlexLog.debug(Arrays.deepToString(MOB_TYPES.toArray()));
                 PlexLog.debug("A genius tried to remove a mob that doesn't exist: " + args[0].toUpperCase());
                 sender.sendMessage(messageComponent("notAValidMobButValidEntity"));
                 return null;
             }
         }
-        if (type != null) {
+        if (type != null)
+        {
             mobName = WordUtils.capitalizeFully(type.name().replace("_", " "));
             PlexLog.debug("The args aren't null so the mob is: " + mobName);
         }
         int count = purgeMobs(type);
-        if (type != null) {
+        if (type != null)
+        {
             PlexUtils.broadcast(messageComponent("removedEntitiesOfTypes", sender.getName(), count, mobName));
             PlexLog.debug("All " + count + " of " + mobName + " were removed");
-        } else {
+        }
+        else
+        {
             PlexUtils.broadcast(messageComponent("removedMobs", sender.getName(), count));
             PlexLog.debug("All " + count + " valid mobs were removed");
         }
@@ -64,18 +74,22 @@ public class MobPurgeCMD extends PlexCommand {
         return null;
     }
 
-    public static String multipleS(int count) {
+    private String multipleS(int count)
+    {
         return (count == 1 ? "" : "s");
     }
 
-    // Removes the mobs.
-
-    public int purgeMobs(EntityType type) {
+    private int purgeMobs(EntityType type)
+    {
         int removed = 0;
-        for (World world : Bukkit.getWorlds()) {
-            for (Entity entity : world.getLivingEntities()) {
-                if (entity instanceof LivingEntity && !(entity instanceof Player)) {
-                    if (type != null && !entity.getType().equals(type)) {
+        for (World world : Bukkit.getWorlds())
+        {
+            for (Entity entity : world.getLivingEntities())
+            {
+                if (entity instanceof LivingEntity && !(entity instanceof Player))
+                {
+                    if (type != null && !entity.getType().equals(type))
+                    {
                         continue;
                     }
                     entity.remove();
@@ -86,19 +100,21 @@ public class MobPurgeCMD extends PlexCommand {
         return removed;
     }
 
-    // Adds a tab completion for /mp so players stop complaining we (mostly me) nuked all their mobs because a filter for some reason was never added by the REAL plex devs. Go figure. -Alco_Rs11
-
-    public static List<String> getAllMobs() {
+    private List<String> getAllMobs()
+    {
         List<String> mobs = new ArrayList<>();
         Arrays.stream(EntityType.values()).filter(EntityType::isAlive).filter(EntityType::isSpawnable).forEach(MOB_TYPES::add);
-        for (EntityType entityType : MOB_TYPES) {
+        for (EntityType entityType : MOB_TYPES)
+        {
             mobs.add(entityType.name());
         }
         return mobs;
     }
 
-    public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException {
-        if (args.length == 1) {
+    public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) throws IllegalArgumentException
+    {
+        if (args.length == 1)
+        {
             return getAllMobs();
         }
         return Collections.emptyList();
