adb0d070a435852d5fbccf6d7cd7e7d6005b2b7d
Telesphoreo <me@telesphoreo.me>
2024-01-25 15:56:45 -0600

    Update Jetty

--- server/src/main/java/dev/plex/PlexLibraryManager.java
+++ server/src/main/java/dev/plex/PlexLibraryManager.java
@@ -29,9 +29,9 @@ public class PlexLibraryManager implements PluginLoader
         // The plugin is null, a hacky way to check whether to load Jetty or not
         if (new File("plugins/Plex/modules/Module-HTTPD.jar").isFile())
         {
-            resolver.addDependency(new Dependency(new DefaultArtifact("org.eclipse.jetty:jetty-server:11.0.18"), null));
-            resolver.addDependency(new Dependency(new DefaultArtifact("org.eclipse.jetty:jetty-servlet:11.0.18"), null));
-            resolver.addDependency(new Dependency(new DefaultArtifact("org.eclipse.jetty:jetty-proxy:11.0.18"), null));
+            resolver.addDependency(new Dependency(new DefaultArtifact("org.eclipse.jetty:jetty-server:11.0.19"), null));
+            resolver.addDependency(new Dependency(new DefaultArtifact("org.eclipse.jetty:jetty-servlet:11.0.19"), null));
+            resolver.addDependency(new Dependency(new DefaultArtifact("org.eclipse.jetty:jetty-proxy:11.0.19"), null));
         }
         classpathBuilder.addLibrary(resolver);
     }
