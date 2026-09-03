68c4251d1ae496286fb4bf94785bd4de9d64dc37
Telesphoreo <me@telesphoreo.me>
2026-09-03 17:41:11 -0400

    Update build.gradle.kts

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
     compileOnlyApi("org.jdbi:jdbi3-core:3.54.0")
     api("com.google.code.gson:gson:2.14.0")
-    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+    compileOnlyApi("io.papermc.paper:paper-api:26.2.build.+")
     compileOnly("org.apache.logging.log4j:log4j-api:2.26.1")
     compileOnly("org.jetbrains:annotations:26.1.0")
 }
